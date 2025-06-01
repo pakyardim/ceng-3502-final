c71cc19329e7595636b9c412f16274eb63d55ceb
pakyardim <emirhanpakyardim@gmail.com>
2025-06-01 13:11:09 +0300

    init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>FlyTicket</h1>
+      <div className="bg-amber-500">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
